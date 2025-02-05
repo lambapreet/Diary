c4dacdca56134caa14164f220d234c19615d54d2
Preet <Preet@gmail.com>
2025-02-05 14:42:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Entry
+
+# Mixin to require login for all views that use it
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+# List of entries
+class EntryListView(LockedView, ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+# Detail view for a single entry
+class EntryDetailView(LockedView, DetailView):
+    model = Entry
+
+# Create a new entry
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+
+# Update an existing entry
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry was updated!"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "entry-detail",
+            kwargs={"pk": self.object.pk}
+        )
+
+# Delete an entry
+class EntryDeleteView(LockedView, SuccessMessageMixin, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
